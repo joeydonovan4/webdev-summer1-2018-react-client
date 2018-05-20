3191ee83645e4319fc62d44f28e4dd7e2d5628f0
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 17:25:58 -0400

    Skeleton for Module List

---
+++ src/containers/ModuleList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class ModuleList extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Module List</h2>
+                <ul className="list-group">
+                </ul>
+            </div>
+        )
+    }
+}
+export default ModuleList;
