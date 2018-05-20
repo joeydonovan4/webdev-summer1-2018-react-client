b5919ddc84c29f4412f9b15dc4614332856c1705
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 17:10:58 -0400

    Add CourseList

---
+++ src/containers/CourseList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class CourseList extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Course List</h2>
+                <table className="table">
+                </table>
+            </div>
+        )
+    }
+}
+export default CourseList;
