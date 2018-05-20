d7805a48c66e18fc038efab01ace24ee3fdb7450
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 17:19:01 -0400

    Skeleton for CourseEditor

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class CourseEditor extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Course Editor</h2>
+            </div>
+        )
+    }
+}
+export default CourseEditor;
