c55df0c8db2c17608a33c7c1af2a6b16d714f86d
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 18:31:13 -0400

    Endpoints for Module

--- src/services/ModuleServiceClient.js
+++ src/services/ModuleServiceClient.js
@@ -1,6 +1,50 @@
+let _singleton = Symbol();
 const MODULE_URI = '/api/modules';
 
 class ModuleServiceClient {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Cannot instantiate directly.');
+    }
 
+    static get instance() {
+        if (!this[_singleton])
+            this[_singleton] = new ModuleServiceClient(_singleton);
+        return this[_singleton];
+    }
+
+    findAllModules() {
+        return fetch(MODULE_URI)
+            .then((resp) => {
+                return resp.json();
+            });
+    }
+
+    deleteModule(id) {
+        return fetch(MODULE_URI + '/' + id, {
+            method: 'DELETE'
+        }).then((resp) => {
+            return resp.json();
+        });
+    }
+
+    findModuleById(id) {
+        return fetch(MODULE_URI + '/' + id)
+            .then((resp) => {
+                return resp.json();
+            });
+    }
+
+    updateModule(id, mod) {
+        return fetch(MODULE_URI + '/' + id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(mod)
+        }).then((resp) => {
+            return resp.json();
+        })
+    }
 }
 export default ModuleServiceClient;
