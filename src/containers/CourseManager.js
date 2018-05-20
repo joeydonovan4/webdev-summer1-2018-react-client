ee9b97b11f77006eaa4b6a4f3d7d267fb81a7028
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 12:43:56 -0400

    Course manager container

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+class CourseManager extends Component {
+    render() {
+        return (
+            <h1>Course Manager</h1>
+        )
+    }
+}
+export default CourseManager;
