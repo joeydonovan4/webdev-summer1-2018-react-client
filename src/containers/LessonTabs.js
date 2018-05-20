489094a38dff8f8f25f9b3a01219c558efd83e9e
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 17:33:03 -0400

    Skeleton for Lesson tabs

---
+++ src/containers/LessonTabs.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+class LessonTabs extends Component {
+    render() {
+        return (
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        Example Tab
+                    </a>
+                </li>
+            </ul>
+        )
+    }
+}
+export default LessonTabs;
