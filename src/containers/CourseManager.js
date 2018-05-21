81c202bcce89ace1adb82bddd3fac8f024eeb38f
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-21 15:27:17 -0400

    Render CourseManager with NavBar

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import NavBar from '../components/NavBar';
+import CourseList from './CourseList';
+import CourseEditor from './CourseEditor';
+
 class CourseManager extends Component {
     render() {
         return (
-            <h1>Course Manager</h1>
+            <Router>
+                <div>
+                    <NavBar/>
+
+                    <Route path="/courses" component={CourseList}></Route>
+                    <Route path="/courses/:courseId" component={CourseEditor}></Route>
+                </div>
+            </Router>
         )
     }
 }
