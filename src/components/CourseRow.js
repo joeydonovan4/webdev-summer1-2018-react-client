391c33c09375fe9dd59d685137d614e62dbd2b59
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-21 18:05:09 -0400

    Update CourseRow

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 class CourseRow extends Component {
     render() {
         return (
-            <tr></tr>
+            <tr>
+                <td>
+                    <Link to={`/courses/${this.props.course.id}`}>
+                        {this.props.course.title}
+                    </Link>
+                </td>
+                <td>
+                    Me
+                </td>
+                <td>
+                    {this.props.course.updatedAt}
+                </td>
+                <td>
+                </td>
+            </tr>
         )
     }
 }
