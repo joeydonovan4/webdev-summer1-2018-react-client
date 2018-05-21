33d0fe73472bc63201e538dcb93dcb9b42ed2efe
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-21 18:05:54 -0400

    Update CourseList

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,12 +1,52 @@
 import React, {Component} from 'react';
+import {Table} from 'react-bootstrap';
+import '../styles/CourseList.css';
+import CourseServiceClient from '../services/CourseServiceClient';
+import CourseRow from '../components/CourseRow';
 class CourseList extends Component {
+    constructor() {
+        super();
+        this.courseService = CourseServiceClient.instance;
+    }
+
+    componentDidMount() {
+        this.findAllCourses();
+    }
+
+    findAllCourses() {
+        this.courseService.findAllCourses()
+            .then((courses) => {
+                this.setState({courses: courses});
+            });
+    }
+
+    renderCourses() {
+        let courses = null;
+        if (this.state) {
+            courses = this.state.courses.map((course) => {
+                return <CourseRow key={course.id} course={course}/>
+            });
+        }
+        return (
+            courses
+        )
+    }
+
     render() {
         return (
-            <div>
-                <h2>Course List</h2>
-                <table className="table">
-                </table>
-            </div>
+            <Table responsive hover>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Owned By</th>
+                        <th>Last Modified</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderCourses()}
+                </tbody>
+            </Table>
         )
     }
 }
