bb3049f7317b412cbe2942a570462fcbd979f6a3
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-21 16:22:56 -0400

    Change background color of body

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import CourseManager from './containers/CourseManager';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+document.body.style.backgroundColor = '#dad9d9';
 ReactDOM.render(
-    <CourseManager/>,
+    <div className="container-fluid">
+        <CourseManager/>
+    </div>,
     document.getElementById('root')
 );
